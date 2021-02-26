5f3e3ac5974c573f2cd8d8d15e6d9b80c8f0e420
Bryan Powell <b.powell@salesforce.com>
2021-02-26 12:24:20 -0800

    build: runWithRetry top-level

--- scripts/verify-test-results.js
+++ scripts/verify-test-results.js
@@ -24,6 +24,43 @@ function resultsGeneratedIfTestsPresent(packagePath, category) {
   return true;
 }
 
+async function runWithRetry(testFn, maxRetry = 3) {
+  let retry = 0;
+  const cwd = process.cwd();
+
+  do {
+    await testFn();
+
+    const resultsGenerated = resultsGeneratedIfTestsPresent(
+      cwd,
+      'vscode-integration'
+    );
+
+    if (resultsGenerated) {
+      return;
+    }
+
+    retry += 1;
+    console.log(
+      `WARNING: Tests results not generated...retrying (${retry}/${MAX_RETRY})`
+    );
+  } while (retry < maxRetry);
+
+  missingMessage = `Missing junit results for the ${path.basename(
+    path.dirname(cwd)
+  )} package!\n`;
+  missingMessage += '\n\nPossible Issues:\n\n';
+  missingMessage +=
+    "1) Tests in the expected suite categories haven't run yet (unit, integration, etc.).\n";
+  missingMessage +=
+    '2) An unexpected test runner or reporter failure while running tests. Sometimes extension activation issues or issues in the tests can silently fail.\n';
+  missingMessage += '3) Test run configuration is improperly set up.\n';
+  missingMessage += '4) Circular imports.\n';
+  console.error(missingMessage);
+  process.exit(1);
+}
+
 module.exports = {
-  resultsGeneratedIfTestsPresent
+  resultsGeneratedIfTestsPresent,
+  runWithRetry
 };
