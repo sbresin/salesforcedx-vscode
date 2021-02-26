9771a2781536cf9b11d9f7bd75b40f448bc2f953
Bryan Powell <b.powell@salesforce.com>
2021-02-26 12:54:43 -0800

    build: runWithRetry lwc

--- scripts/run-tests-with-recipes.js
+++ scripts/run-tests-with-recipes.js
@@ -1,18 +1,22 @@
 #!/usr/bin/env node
 
 const path = require('path');
+const { runWithRetry } = require('./verify-test-results');
 const { runIntegrationTests } = require('./vscode-integration-testrunner');
 
 const cwd = process.cwd();
-runIntegrationTests({
-  extensionDevelopmentPath: path.join(__dirname, '..', 'packages'),
-  extensionTestsPath: path.join(cwd, 'out', 'test', 'vscode-integration'),
-  testWorkspace: path.join(
-    __dirname,
-    '..',
-    'packages',
-    'system-tests',
-    'assets',
-    'lwc-recipes'
-  )
-});
+
+runWithRetry(() =>
+  runIntegrationTests({
+    extensionDevelopmentPath: path.join(__dirname, '..', 'packages'),
+    extensionTestsPath: path.join(cwd, 'out', 'test', 'vscode-integration'),
+    testWorkspace: path.join(
+      __dirname,
+      '..',
+      'packages',
+      'system-tests',
+      'assets',
+      'lwc-recipes'
+    )
+  })
+);
