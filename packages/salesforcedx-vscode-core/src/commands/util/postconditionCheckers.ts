8b3544be9cca150900d21b04f59c50c09bb648e9
Violet Yao <vyao@salesforce.com>
2021-06-24 16:08:20 -0700

    feat: ui enhancement

--- packages/salesforcedx-vscode-core/src/commands/util/postconditionCheckers.ts
+++ packages/salesforcedx-vscode-core/src/commands/util/postconditionCheckers.ts
@@ -275,7 +275,7 @@ export class ConflictDetectionChecker implements PostconditionChecker<string> {
         )
       );
       results.different.forEach(file => {
-        channelService.appendLine(normalize(file));
+        channelService.appendLine(normalize(file.path));
       });
       channelService.showChannelOutput();
 
@@ -373,7 +373,7 @@ export class TimestampConflictChecker implements PostconditionChecker<string> {
         )
       );
       results.different.forEach(file => {
-        channelService.appendLine(normalize(file));
+        channelService.appendLine(normalize(file.path));
       });
       channelService.showChannelOutput();
 
